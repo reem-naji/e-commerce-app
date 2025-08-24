1b11a82afa782011425a38b2e5d996b089e787f1
Reem Naji <reemalee7@gmail.com>
2025-08-24 18:15:03 +0300

    initial navigation bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Products } from './products/products';
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Products],
+  imports: [RouterOutlet, Products, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
