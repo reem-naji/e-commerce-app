ba9f4d9290f964d8686bb1a69abf4fd168dfe3e3
Reem Naji <reemalee7@gmail.com>
2025-08-25 11:47:26 +0300

    added fontawesome module for navbar icons

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faShopify } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
-
+  faShopify = faShopify;
+  faUser = faUser;
+  faCartShopping = faCartShopping;
+  faMagnifyingGlass = faMagnifyingGlass;
 }
